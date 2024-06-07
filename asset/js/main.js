a7c7fee8259ea7943b7c2c96ec6ff1fcd9d5ddc8
ownkhan125 <107195320+ownkhan125@users.noreply.github.com>
2024-06-07 12:09:59 -0700

    update"

--- asset/js/main.js
+++ asset/js/main.js
@@ -142,10 +142,7 @@ function checkVisibility() {
 function isElementInViewport(el) {
     var rect = el.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
 
